a7abfb56a740317c970aee50b5f744abe9eda1ac
John Huang <johnh2@gmail.com>
2018-03-06 23:03:59 -0800

    first draft

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './app.css';
+
+class App extends Component {
+    render() {
+        return <h1>Hello from React!!</h1>
+    }
+}
+
+export default App;
